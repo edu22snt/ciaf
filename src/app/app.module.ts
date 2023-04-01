0245cbf4fb48de9ddc014922cf31c358444ef3b0
C1300121 Eduardo Monteiro de Andrade <c1300121@interno.bb.com.br>
2023-04-01 08:17:09 -0300

    criação das funcionalidades aluno, professor, financeiro e login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AlunoComponent } from './aluno/aluno.component';
+import { ProfessorComponent } from './professor/professor.component';
+import { FinanceiroComponent } from './financeiro/financeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AlunoComponent,
+    ProfessorComponent,
+    FinanceiroComponent
   ],
   imports: [
     BrowserModule,
